bf3ac44f2ab40294e956655780777a755a3ea6fc
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-04-08 14:51:21 +0700

    Use numpy, write CSV synchronously

--- log_adcs.py
+++ log_adcs.py
@@ -1,68 +1,61 @@
 from __future__ import annotations
-from typing import AsyncIterator, List
+from typing import List, TextIO, Generator
 
-import os
 from pathlib import Path
 from dataclasses import dataclass
-from contextlib import AsyncExitStack, asynccontextmanager
+from contextlib import AsyncExitStack, contextmanager
 from datetime import datetime
 import argparse
 
+import numpy as np
+from numpy.typing import NDArray
+
 import asyncio
-from aioitertools.builtins import zip as azip
-import aiofiles
-from aiocsv import AsyncWriter # type: ignore
+from aioitertools.builtins import zip as azip, enumerate as aenumerate
 
 from pyepics_asyncio import Pv
 
 from utils import tornado
 
-AsyncTextIO = aiofiles.threadpool.text.AsyncTextIOWrapper
-
-Waveform = List[float]
+Waveform = NDArray[np.float64]
 
 
 @dataclass
 class Storage:
-    file: AsyncTextIO
-    writer: AsyncWriter
+    file: TextIO
     start_time: datetime
 
     @staticmethod
-    @asynccontextmanager
-    async def open(path: Path) -> AsyncIterator[Storage]:
-        async with aiofiles.open(path, mode="w", encoding="utf-8", newline="") as file:
-            yield await Storage.__ainit__(file)
+    @contextmanager
+    def open(path: Path) -> Generator[Storage, None, None]:
+        with open(path, "w") as file:
+            yield Storage(file)
 
-    @staticmethod
-    async def __ainit__(file: AsyncTextIO) -> Storage:
-        writer = AsyncWriter(file)
+    def __init__(self, file: TextIO) -> None:
+        self.file = file
+        self.start_time = datetime.now()
+        # writerow(["time, s"] + [f"aai{i}, V" for i in range(tornado.ADC_CHANNEL_COUNT)])
 
-        await writer.writerow(["time, s"] + [f"aai{i}, V" for i in range(tornado.ADC_CHANNEL_COUNT)])
-        await file.flush()
-
-        return Storage(file, writer, datetime.now())
-
-    async def write(self, waveforms: List[Waveform]) -> None:
+    def write(self, waveforms: List[Waveform]) -> float:
         now = (datetime.now() - self.start_time).total_seconds()
-        for i, values in enumerate(zip(*waveforms)):
-            time = now + i * tornado.ADC_PERIOD_S
-            await self.writer.writerow([f"{time:.3f}"] + [f"{v:.6f}" for v in values])
-
+        time = now + np.arange(len(waveforms[0]), dtype=np.float64) * tornado.ADC_PERIOD_S
+        content: NDArray[np.float64] = np.stack([time, *waveforms], axis=-1)
+        np.savetxt(self.file, content, fmt="%.6f", delimiter=",")
         self.file.flush()
+        return now
 
 
 async def store(path: Path) -> None:
-    async with Storage.open(path) as storage:
+    with Storage.open(path) as storage:
         pvs = [await Pv.connect(f"aai{i}") for i in range(tornado.ADC_CHANNEL_COUNT)]
         async with AsyncExitStack() as stack:
             monitors = [await stack.enter_async_context(pv.monitor()) for pv in pvs]
-            async for wfs in azip(*monitors):
-                print(f"Write waveforms")
-                await storage.write(list(wfs))
+            async for i, wfs in aenumerate(azip(*monitors)):
+                now = storage.write(list(wfs))
+                print(f"Writing waveforms #{i} at {now} secs")
 
 
-def main() -> None:
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Log ADC channels to file.")
     parser.add_argument(
         "-o",
@@ -77,8 +70,4 @@ def main() -> None:
     out_path = args.out_dir / f"log_adcs_{date_fmt}.csv"
 
     print(f"Logging ADC measurements to file '{out_path}'")
-    asyncio.run(store(out_path), debug=True)
-
-
-if __name__ == "__main__":
-    main()
+    asyncio.run(store(out_path))
